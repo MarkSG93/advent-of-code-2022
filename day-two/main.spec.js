35c5a4d1af13e2270c07500019288ff4af33e253
Mark Griffiths <mark.griffiths@cazoo.co.uk>
2022-12-05 15:59:13 +0000

    Add play map to more easily calculate score

--- day-two/main.spec.js
+++ day-two/main.spec.js
@@ -19,47 +19,55 @@ const resultPoints = {
   "Loss": 0
 }
 
+const playByPlay = {
+  "Boulder": { "Boulder": "Draw", "Parchment": "Loss", "Shears": "Win" },
+  "Parchment": { "Boulder": "Win", "Parchment": "Draw", "Shears": "Loss" },
+  "Shears": { "Boulder": "Loss", "Parchment": "Win", "Shears": "Draw" },
+}
+
 function rockPaperScissors(playerOne, playerTwo) {
   const playerOnePlay = inputToMeaning[playerOne];
   const playerTwoPlay = inputToMeaning[playerTwo];
 
-  let result = "Loss";
+  let result = playByPlay[playerTwoPlay][playerOnePlay];
   let playerTwoPoints = playPoints[playerTwoPlay];
-
-  if (playerOnePlay === "Boulder" && playerTwoPlay === "Boulder") {
-    result = "Draw";
-  }
-
-  if (playerOnePlay === "Boulder" && playerTwoPlay === "Shears") {
-    result = "Loss";
-  }
-
-  if (playerOnePlay === "Boulder" && playerTwoPlay === "Parchment") {
-    result = "Win";
-  }
 
   return playerTwoPoints + resultPoints[result];
 }
 
 describe("Boulder, Parchment, Shears", () => {
   describe("Given player one chooses Boulder and you choose Parchment", () => {
-    it("You receives 8 playPoints", () => {
+    it("You receives 8 points", () => {
       const result = rockPaperScissors("A", "Y")
       expect(result).toEqual(8);
     });
   });
 
   describe("Given player one chooses Boulder and you choose Boulder", () => {
-    it("You receive 4 playPoints", () => {
+    it("You receive 4 points", () => {
       const result = rockPaperScissors("A", "X")
       expect(result).toEqual(4);
     });
   });
 
   describe("Given player one chooses Boulder and you choose Shears", () => {
-    it("You receive 3 playPoints", () => {
+    it("You receive 3 points", () => {
       const result = rockPaperScissors("A", "Z")
       expect(result).toEqual(3);
+    });
+  });
+
+  describe("Given player one chooses Parchment and you choose Boulder", () => {
+    it("You receive 1 points", () => {
+      const result = rockPaperScissors("B", "X");
+      expect(result).toEqual(1);
+    });
+  });
+
+  describe("Given player one chooses Shears and you choose Boulder", () => {
+    it("You receive 1 points", () => {
+      const result = rockPaperScissors("C", "X");
+      expect(result).toEqual(7);
     });
   });
 });
